b38d038d555f596563b23874bdea1b9f94315116
Ruihan Zhao <ruihan.zhao@utexas.edu>
2023-08-14 23:33:36 -0500

    train with policy reuse

--- train_baseline_sac.py
+++ train_baseline_sac.py
@@ -6,7 +6,7 @@ import argparse
 from stable_baselines3 import SAC
 
 
-TOTAL_TRAINING_STEPS = 250000
+TOTAL_TRAINING_STEPS = 1000000
 LOG_INTERVAL = 4
 EVAL_INTERVAL = 2000
 NUM_EVAL_EPISODES = 20
@@ -14,7 +14,9 @@ NUM_EVAL_EPISODES = 20
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env_name', type=str, default='BaselineCompLift-v1')
+    # parser.add_argument('--env_name', type=str, default='BaselineCompLift-v1')
+    # parser.add_argument('--env_name', type=str, default='BaselineCompStack-v1')
+    parser.add_argument('--env_name', type=str, default='BaselineCompPickPlaceCan-v1')
     args = parser.parse_args()
     return args
 
